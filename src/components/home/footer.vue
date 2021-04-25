f60a2d2daf2f5a1c1dc2457bafdd7408816e26bb
舒兴 <sx437181241@qq.com>
2021-04-25 14:49:17 +0800

    sa

--- src/components/home/footer.vue
+++ src/components/home/footer.vue
@@ -1,41 +1,8 @@
 <template>
     <div class="myfooter">
-        <div class="footer-left">
-          <span style="color:#FF620C;">友情链接：</span>
-          <a
-            href="http://www.swcc.org.cn/"
-            target="_blank"
-          >
-            中国水土保持生态网
-          </a>
-         <!--  <a
-            href="https://www.bidcenter.com.cn/newssearchyz-63334594.html"
-            target="_blank"
-          >
-            贵州水土保持监测站
-          </a> -->
-          <a
-            href="http://mwr.guizhou.gov.cn/"
-            target="_blank"
-          >
-            贵州省水利厅
-          </a>
-         
-        </div>
-        <div class="footer-right">
-        <!--   <span>关于我们</span>
-          <span>@贵州东方世纪科技股份有限公司</span>
-          <span>黔公安网 00000000号</span> -->
-          <span>黔ICP备05001357号-7</span>
-          
-          <a href="//bszs.conac.cn/sitename?method=show&amp;id=AF7DD292662E736AE05310291AACB2A5" target="_blank"><img id="imgConac" vspace="0" hspace="0" border="0" src="../../assets/administrate.png" data-bd-imgshare-binded="1"></a>
-        </div>
-        <!-- <div>
-          <img  src="../../assets/administrate.png">
-        </div> -->
+        
     </div>
 </template>
-<script type="text/javascript">document.write(unescape("<span id='ideConac' ></span><script src='http://dcs.conac.cn/js/24/000/0000/40707124/CA240000000407071240007.js' type='text/javascript'></script>"));</script>
 <script>
   export default {
     name: 'Head',
--- src/router/index.js
+++ src/router/index.js
@@ -3,26 +3,6 @@ import Router from 'vue-router'
 //import HelloWorld from '@/components/HelloWorld'
 import Home from '@/pages/home'
 import Personal from '@/pages/personal'
-import News from '@/pages/news'
-import Datas from '@/pages/datas'
-import Show from '@/pages/newshow'
-import Garden from '@/pages/garden'
-import Popular from '@/pages/popular'
-import Intro from '@/pages/intro'
-
-import NewsDetail from '@/pages/newsDetail'
-import PopularDetail from '@/pages/popularDetail'
-import DatasDetail from '@/pages/datasDetail'
-import DatasDetail1 from '@/pages/datasDetail1'
-import GardenDetail from '@/pages/gardenDetail'
-import ShowDetail from '@/pages/showDetail'
-
-import Vdata1 from '@/pages/vdata1'
-import Vdata2 from '@/pages/vdata2'
-import Vdata3 from '@/pages/vdata3'
-import Vdata4 from '@/pages/vdata4'
-import Test from '@/pages/test'
-import Olmap from '@/pages/olmap'
 
 import Login from '@/pages/personal/login'
 import Register from '@/pages/personal/register'
@@ -35,6 +15,14 @@ Vue.use(Router)
 const HoTitle = '-贵州水土保持-数据共享中心';
 const router = new Router({
   routes: [
+    {
+      path: '/personal',
+      name: 'Personal',
+      component: Personal,
+      meta:{
+          title: 'Personal'
+      }
+    },
     {
       path: '/Login',
       name: 'Login',
@@ -70,22 +58,6 @@ const router = new Router({
       }
     },
     {
-      path: '/test',
-      name: 'test',
-      component: Test,
-      meta:{
-          title: '注册'+HoTitle
-      }
-    },
-     {
-      path: '/ForgetPwd',
-      name: 'ForgetPwd',
-      component: ForgetPwd,
-      meta:{
-          title: '忘记密码'+HoTitle
-      }
-    },
-    {
       path: '/',
       name: 'Home',
       component: Home,
@@ -93,150 +65,6 @@ const router = new Router({
           title: '首页'+HoTitle
       }
     },
-    {
-      path: '/olmap',
-      name: 'olmap',
-      component: Olmap,
-      meta:{
-          title: '专题地图展示'+HoTitle
-      }
-    },
-    {
-      path: '/Vdata1',
-      name: 'Vdata1',
-      component: Vdata1,
-      meta:{
-          title: '治理大数据'
-      }
-    },
-    {
-      path: '/Vdata2',
-      name: 'Vdata2',
-      component: Vdata2,
-      meta:{
-          title: '监督大数据'
-        }
-    },
-    {
-      path: '/Vdata3',
-      name: 'Vdata3',
-      component: Vdata3,
-      meta:{
-          title: '站网大数据'
-      }
-    },
-    {
-      path: '/Vdata4',
-      name: 'Vdata4',
-      component: Vdata4,
-      meta:{
-          title: '行政管理'
-      }
-    },
-    {
-      path: '/personal',
-      name: 'Personal',
-      component: Personal,
-      meta:{
-          title: '个人中心'+HoTitle
-      }
-    },
-    {
-      path: '/news',
-      name: 'news',
-      component: News,
-      meta:{
-          title: '新闻动态'+HoTitle
-      }
-    },
-    {
-      path: '/datas',
-      name: 'datas',
-      component: Datas,
-      meta:{
-          title: '数据中心'+HoTitle
-      }
-    },
-    {
-      path: '/show',
-      name: 'show',
-      component: Show,
-      meta:{
-          title: '专题展示'+HoTitle
-      }
-    },
-    {
-      path: '/garden',
-      name: 'garden',
-      component: Garden,
-      meta:{
-          title: '示范园区'+HoTitle
-      }
-    },
-    {
-      path: '/popularDetail',
-      name: 'PopularDetail',
-      component: PopularDetail,
-      meta:{
-          title: '科技推广详情'+HoTitle
-      }
-    },
-    {
-      path: '/popular',
-      name: 'popular',
-      component: Popular,
-      meta:{
-          title: '专题展示'+HoTitle
-      }
-    },
-    {
-      path: '/intro',
-      name: 'intro',
-      component: Intro,
-      meta:{
-          title: '平台简介'+HoTitle
-      }
-    },
-    {
-      path: '/newsDetail',
-      name: 'NewsDetail',
-      component: NewsDetail,
-      meta:{
-          title: '新闻详情'+HoTitle
-      }
-    },
-    {
-      path: '/DatasDetail',
-      name: 'DatasDetail',
-      component: DatasDetail,
-      meta:{
-          title: '数据详情'+HoTitle
-      }
-    },
-    {
-      path: '/DatasDetail1',
-      name: 'DatasDetail1',
-      component: DatasDetail1,
-      meta:{
-          title: '数据详情'+HoTitle
-      }
-    },
-    {
-      path: '/gardenDetail',
-      name: 'gardenDetail',
-      component: GardenDetail,
-      meta:{
-          title: '园区详情'+HoTitle
-      }
-    },
-    {
-      path: '/showDetail',
-      name: 'showDetail',
-      component: ShowDetail,
-      meta:{
-          title: '展示详情'+HoTitle
-      }
-    }
   ]
 })
 
